e9f585b9ba451ceb71ccd6ed479d8347eefd0b4a
KK-Repos <iamkishorekumar1@gmail.com>
2024-09-01 21:18:21 +0530

    Updated notes for output and variable

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "aws" {
   source = "./aws"
-  vm_region = var.aws_Region
+  vm_region = var.aws_Region #Pass variable value from root 
   providers = {
     aws = aws
   }
-  count  = var.provider_choice["key"].name == "aws" ? var.provider_choice["key"].count : 0
+  count  = var.provider_choice["key"].name == "aws" ? var.provider_choice["key"].count : 0 # #Pass variable value from root 
 }
---
+++ notes/outputs.md
@@ -0,0 +1,6 @@
+
+How to access output in root from the module 
+
+Declare Outputs in the child module using the output block.
+Reference the Module in the root module using the module block.
+Access Outputs from the child module in the root module by referring to them using module.<module_name>.<output_name>.
